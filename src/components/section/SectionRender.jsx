b891e78a0955683c2b66cc604df0bfc14e926e58
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-16 16:56:17 +0100

    Update SectionRender.jsx

--- src/components/section/SectionRender.jsx
+++ src/components/section/SectionRender.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import css from './sectionRender.modules.css';
 // import PropTypes from 'prop-types';
 
@@ -6,34 +7,58 @@ import css from './sectionRender.modules.css';
 
 
 
+class Feedback extends React.Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
+countGood = () => { };
+countNeutral = () => { };
+countBad = () => {};
+countTotalFeedback = () => { };
+countPositiveFeedbackPercentage = () => { };
+    render() {
+        return(
+        <section>
+            <div>
+                <h2>Please leave feedback</h2>
+                <button type='button' className={css.button} onClick={this.countGood}>
+                    Good
+                </button>
+                <button type='button' className={css.button} onClick={this.countNeutral}>
+                    Neutral
+                </button>
+                <button type='button' className={css.button} onClick={this.countBad}>
+                    Bad
+                </button>
+            </div>
+                <div>
+                    <h2>Statistics</h2>
+                    <p>Good:
+                        <span className='good__value'>{this.state.good}</span>
+                    </p>
+                    <p>Neutral:
+                        <span className='neutral__value'>{this.state.neutral}</span>
+                    </p>
+                    <p>Bad:
+                        <span className='bad__value'>{this.state.bad}</span>
+                    </p>
+                    <p>Total:
+                        <span className='total__value'>0</span>
+                    </p>
+                    <p>Positive feedback:
+                        <span className='positiveFeedback__value'>0</span>
+                    </p>
+            </div>
+        </section>
+        )
+    }
+}
+    
 
 
-export const sectionRender = (Good, Neutral, Total, Bad, PositiveFeedback,) => {
-  return (
-    <section>
-    <div>
-        <h2>Please leave feedback</h2>
-        <button className={css.button}>
-            Good
-        </button>
-           <button className={css.button}>
-            Neutral
-        </button>
-           <button className={css.button}>
-            Bad
-        </button>
-    </div>   
-    <div>
-        <h2>Statistics</h2>
-        <p>Good: {Good}</p>
-        <p>Neutral: {Neutral}</p>
-        <p>Bad: {Bad}</p>
-        <p>Total: {Total}</p>
-        <p>Positive feedback: {PositiveFeedback}</p>
-    </div>
-</section>
-  );
-};
+
 
 // sectionRender.propTypes = {
 //     Good : PropTypes.number.isRequired,
@@ -42,4 +67,4 @@ export const sectionRender = (Good, Neutral, Total, Bad, PositiveFeedback,) => {
 //     Total: PropTypes.number.isRequired,
 //     PositiveFeedback: PropTypes.number.isRequired,
 // }
-export default sectionRender
+export default Feedback;
