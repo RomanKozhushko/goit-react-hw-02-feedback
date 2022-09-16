e3e420e5d42ffbed42b64dfe984fd6eedada8ebb
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-09-16 15:51:39 +0100

    !

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import SectionRender from "components/section/SectionRender";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+       <SectionRender  />
     </div>
   );
 };
+  export default App
---
+++ src/components/section/SectionRender.jsx
@@ -0,0 +1,45 @@
+import css from './sectionRender.modules.css';
+import PropTypes from 'prop-types';
+
+// countTotalFeedback();
+// countPositiveFeedbackPercentage();
+
+
+
+
+
+export const sectionRender = (Good, Neutral, Total, Bad, PositiveFeedback,) => {
+  return (
+    <section>
+    <div>
+        <h2>Please leave feedback</h2>
+        <button className={css.button}>
+            Good
+        </button>
+           <button className={css.button}>
+            Neutral
+        </button>
+           <button className={css.button}>
+            Bad
+        </button>
+    </div>   
+    <div>
+        <h2>Statistics</h2>
+        <p>Good: {Good}</p>
+        <p>Neutral: {Neutral}</p>
+        <p>Bad: {Bad}</p>
+        <p>Total: {Total}</p>
+        <p>Positive feedback: {PositiveFeedback}</p>
+    </div>
+</section>
+  );
+};
+
+sectionRender.propTypes = {
+    Good : PropTypes.number.isRequired,
+    Neutral :PropTypes.number.isRequired,
+    Bad: PropTypes.number.isRequired,
+    Total: PropTypes.number.isRequired,
+    PositiveFeedback: PropTypes.number.isRequired,
+}
+export default sectionRender
